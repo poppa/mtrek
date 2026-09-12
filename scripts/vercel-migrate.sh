e19e67690d5abdcdc7111056eed277edc056efc4
Pontus Östlund <92401+poppa@users.noreply.github.com>
2026-09-12 07:49:48 +0200

    fix: Build

--- scripts/vercel-migrate.sh
+++ scripts/vercel-migrate.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 
-echo "Env: ${VERCEL_ENV:-}"
-echo "DB URL: ${POSTGRES_URL_NON_POOLING:-}"
-
 if [ "${VERCEL_ENV:-}" = "production" ]; then
   : "${POSTGRES_URL_NON_POOLING:?Set POSTGRES_URL_NON_POOLING for Vercel Production}"
   npm run db:migrate
